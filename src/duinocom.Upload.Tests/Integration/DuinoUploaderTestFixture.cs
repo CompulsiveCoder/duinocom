3db0c44a3edec7e76b20e4a8a5abf639cca981fd
CompulsiveCoder <compulsivecoder@gmail.com>
2015-09-29 11:38:59 +1000

    Added uploader

---
+++ src/duinocom.Upload.Tests/Integration/DuinoUploaderTestFixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+
+namespace duinocom.Upload.Tests
+{
+  [TestFixture]
+  public class DuinoUploaderTestFixture
+  {
+    string blinkSketchCode = @"
+int led = 13;
+
+void setup() {                
+  pinMode(led, OUTPUT);     
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}";
+
+    [Test]
+    public void Test_Upload()
+    {
+      var uploader = new DuinoUploader ();
+     
+      /*var newDirectory = Path.Combine (Environment.CurrentDirectory, "_tmp");
+      Directory.CreateDirectory (newDirectory);
+      newDirectory = Path.Combine (newDirectory, Guid.NewGuid ().ToString ());
+      Directory.CreateDirectory (newDirectory);*/
+
+
+
+      var result = uploader.UploadSketch (Environment.CurrentDirectory, "", "nano328", blinkSketchCode);
+
+      if (uploader.Error.Length > 0) {
+        Console.WriteLine ("Error:");
+        Console.WriteLine (uploader.Error);
+      }
+
+      Assert.AreEqual (0, uploader.Error.Length, "Error: " + uploader.Error);
+
+      Console.WriteLine ("Check your arduino. Is it blinking? It should be.");
+    }
+  }
+}
+
---
+++ src/duinocom.Upload/DuinoUploader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace duinocom.Upload
+{
+  public class DuinoUploader
+  {
+    public string Error = "";
+
+    public DuinoUploader ()
+    {
+    }
+
+    public void InstallOnUbuntu()
+    {
+      ExecuteCommand ("apt-get mono-runtime");
+    }
+
+    public string UploadSketch(string directory, string port, string board, string code)
+    {
+      var tmpDir = Path.GetFullPath ("_tmp");
+
+      var uniqueDir = Path.Combine (tmpDir, Guid.NewGuid ().ToString ());
+
+      var sketchDir = Path.Combine (uniqueDir, "sketch");
+
+      var srcDir = Path.Combine (sketchDir, "src");
+
+      Directory.CreateDirectory (srcDir);
+
+      Directory.CreateDirectory (tmpDir);
+      Directory.CreateDirectory (uniqueDir);
+      Directory.CreateDirectory (sketchDir);
+
+      Directory.SetCurrentDirectory (sketchDir);
+
+      return UploadSketchFile (directory, port, board, sketchDir);
+    }
+
+
+      public string UploadSketchFile(string directory, string port, string board, string sketchDirectoryPath)
+      {
+        var sketchPath = Path.Combine (sketchDirectoryPath, "sketch.ino");
+
+        var output = "";
+
+        ExecuteCommand("ino init");
+    
+        File.WriteAllText (sketchPath, sketchDirectoryPath);
+
+        output += ExecuteCommand("ino build -m " + board);
+
+        // TODO: Enable port parameter
+        output += ExecuteCommand("ino upload -m " + board);// + " -p " + port;
+      
+        Console.WriteLine("Finished");
+
+        if (output.IndexOf("No device matching following was found") > -1)
+        {
+          Error = "No duino compatible device deteceted. Is it plugged in?";
+        }
+
+        return output;
+    }
+
+    public string ExecuteCommand(string command)
+    {
+      try
+      {
+        var spacePos = command.IndexOf (" ");
+        var firstPart = command.Substring (0, spacePos);
+        var secondPart = command.Substring (spacePos+1, command.Length - spacePos-1);
+
+        var startInfo = new ProcessStartInfo(firstPart, secondPart);
+        startInfo.UseShellExecute = false;
+        startInfo.RedirectStandardOutput = true;
+        var process = Process.Start(startInfo);
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+        Console.WriteLine(output);
+        return output + Environment.NewLine;
+      }
+      catch(Exception ex)
+      {
+        string output = "Error" + Environment.NewLine;
+        output += ex.ToString () + Environment.NewLine;
+        Error += ex.ToString ();
+        return output + Environment.NewLine;
+      }
+    }
+  }
+}
+
