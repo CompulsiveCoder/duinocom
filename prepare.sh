ebe9b284461a7cbbab88617a653b9b780e12c1df
CompulsiveCoder <compulsivecoder@gmail.com>
2017-02-04 12:04:42 +1100

    Added scripts

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 echo "Preparing for duinocom project"
 echo "Dir: $PWD"
 
-sudo apt-get update
-sudo apt-get install -y git wget mono-complete
+sudo apt-get update &&
+sudo apt-get install -y git wget mono-complete &&
 
 sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
---
+++ test-via-docker.sh
@@ -0,0 +1,12 @@
+#TEST_CATEGORY=$1
+
+# TODO: Is this needed? Currently all tests are run, and the category is ignored. Remove or reimplement.
+#if [ -z "$TEST_CATEGORY" ]; then
+#    TEST_CATEGORY="Unit"
+#fi
+
+#echo "Tests: $TEST_CATEGORY"
+
+sh build.sh
+
+docker run -i -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/duinocom compulsivecoder/ubuntu-mono /bin/bash -c "cd /duinocom && sh test-all.sh" #$TEST_CATEGORY"
